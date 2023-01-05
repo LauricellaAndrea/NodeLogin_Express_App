5d507ce3f89254426c2e185701666e24c862cfbf
LauricellaAndrea <andrealauricella97@gmail.com>
2023-01-05 15:37:22 +0100

    add design for login

--- login_system/server.js
+++ login_system/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// set the view engine to ejs
 app.set('view engine', 'ejs');
+
+//Caricare risorse statiche per usare css
+app.use('/static', express.static(path.join(__dirname, 'public')))
+//Caricare risorse statiche per usare l'immagine di background
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
 //home route
 app.get('/', (req, res) => {
